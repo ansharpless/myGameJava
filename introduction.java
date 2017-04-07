13577186a5789ea7d0174618b2ba25bd699a31d8
Adrian Sharpless <ansharpless@student.ysu.edu>
2017-04-07 10:14:10 -0400

    Add files via upload

---
+++ introduction.java
@@ -0,0 +1,15 @@
+//This will run an introduction file from the disk
+//Author Adrian Sharpless
+//Last modification date: 4/4/2107
+class introduction {
+
+public static void main(String[] args){
+    System.out.println("This is a test");
+    }//end method main
+
+
+
+
+
+
+}//end class introduction
---
+++ mainGame.java
@@ -0,0 +1,24 @@
+//This is the main game code
+//Author Adrian SHarpless
+// Last modification dat 4/4/2017
+
+class mainGame{
+
+
+public static void main(String[] args){
+    System.out.println("This is a test");
+    /*int monHP,
+        monAT,
+        playerHP,
+        playerAT; TO BE USED WHEN battleLoop class works*/
+    //introduction();//runs first
+    //gameStore();//rns depending on player location
+    //playerLocation();//may need to incorporate battleLoop();
+    //battleLoop()://runs randomly as player reaches certain amount of steps
+
+    }//end method main
+
+
+
+
+}//end class mainGame
